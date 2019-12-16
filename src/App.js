e49683435680afff0237e8566388c8cb2b3a940e
Sam O-A <yibaah@gmail.com>
2019-12-16 15:07:36 -0500

    plus+ 01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import Header from "./components/common/Header";
 import "./App.css";
+import PageNotFound from "./components/PageNotFound";
+import CoursesPage from "./components/courses/CoursesPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>save to reload.</p>
-      </header>
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="jumbotron">
+      <p>
+        This app uses React, Redux, React Router, and many other helpful
+        libraries.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
